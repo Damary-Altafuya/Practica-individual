12f03074a7d9aeaf631910559525966da2e06063
Damary Altafuya <139515688+Damary-Altafuya@users.noreply.github.com>
2023-11-21 23:29:08 -0500

    Add files via upload
    La practica se subió en el GitHub grupal, pero aquí adjunto mi practica en el GitHub personal, ya le comunique en la hora de clase sobre la practica.

---
+++ src/domain/dtos/index.ts
@@ -0,0 +1,15 @@
+
+export * from './comprador/create-comprador.dto';
+export * from './comprador/update-comprador.dto';
+export * from './producto/create-producto.dto';
+export * from './producto/update-producto.dto';
+export * from './transaccion/create-transaccion.dto';
+export * from './transaccion/update-transaccion.dto';
+export * from './usuario/create-usuario.dto';
+export * from './usuario/update-usuario.dto';
+export * from './artesano/create-artesano.dto';
+export * from './artesano/update-artesano.dto';
+export * from './categoria/create-categoria.dto';
+export * from './categoria/update-categoria.dto';
+export * from './carrito/create-carrito.dto';
+export * from './carrito/update-carrito.dto';
